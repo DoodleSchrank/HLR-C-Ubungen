a373f1bacb28d441f3f15d2f3b61dfaec642e274
Yannik Könneker <y.koenneker@web.de>
2019-11-05 21:46:33 +0100

    aufgabe 04

--- 04-PDE/partdiff.c
+++ 04-PDE/partdiff.c
@@ -1,15 +1,15 @@
 /****************************************************************************/
 /****************************************************************************/
 /**                                                                        **/
-/**                TU München - Institut fuer Informatik                   **/
+/**                 TU München - Institut für Informatik                   **/
 /**                                                                        **/
 /** Copyright: Prof. Dr. Thomas Ludwig                                     **/
 /**            Andreas C. Schmidt                                          **/
 /**                                                                        **/
-/** File:      partdiff-seq.c                                              **/
+/** File:      partdiff.c                                                  **/
 /**                                                                        **/
-/** Purpose:   Partial differential equation solver for Gauss-Seidel and   **/
-/**            Jacobi methods.                                             **/
+/** Purpose:   Partial differential equation solver for Gauß-Seidel and    **/
+/**            Jacobi method.                                              **/
 /**                                                                        **/
 /****************************************************************************/
 /****************************************************************************/
@@ -17,27 +17,32 @@
 /* ************************************************************************ */
 /* Include standard header file.                                            */
 /* ************************************************************************ */
+#define _POSIX_C_SOURCE 200809L
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <inttypes.h>
 #include <math.h>
 #include <malloc.h>
 #include <sys/time.h>
-#include "partdiff-seq.h"
+
+#include "partdiff.h"
 
 struct calculation_arguments
 {
-	int     N;              /* number of spaces between lines (lines=N+1)     */
-	int     num_matrices;   /* number of matrices                             */
-	double  ***Matrix;      /* index matrix used for addressing M             */
-	double  *M;             /* two matrices with real values                  */
-	double  h;              /* length of a space between two lines            */
+	uint64_t  N;              /* number of spaces between lines (lines=N+1)     */
+	uint64_t  num_matrices;   /* number of matrices                             */
+	double    h;              /* length of a space between two lines            */
+	double    ***Matrix;      /* index matrix used for addressing M             */
+	double    *M;             /* two matrices with real values                  */
 };
 
 struct calculation_results
 {
-	int     m;
-	int     stat_iteration; /* number of current iteration                    */
-	double  stat_precision; /* actual precision of all slaves in iteration    */
+	uint64_t  m;
+	uint64_t  stat_iteration; /* number of current iteration                    */
+	double    stat_precision; /* actual precision of all slaves in iteration    */
 };
 
 /* ************************************************************************ */
@@ -54,11 +59,11 @@ struct timeval comp_time;        /* time when calculation completed             
 /* ************************************************************************ */
 static
 void
-initVariables (struct calculation_arguments* arguments, struct calculation_results* results, struct options* options)
+initVariables (struct calculation_arguments* arguments, struct calculation_results* results, struct options const* options)
 {
-	arguments->N = options->interlines * 8 + 9 - 1;
+	arguments->N = (options->interlines * 8) + 9 - 1;
 	arguments->num_matrices = (options->method == METH_JACOBI) ? 2 : 1;
-	arguments->h = (float)( ( (float)(1) ) / (arguments->N));
+	arguments->h = 1.0 / arguments->N;
 
 	results->m = 0;
 	results->stat_iteration = 0;
@@ -72,7 +77,7 @@ static
 void
 freeMatrices (struct calculation_arguments* arguments)
 {
-	int i;
+	uint64_t i;
 
 	for (i = 0; i < arguments->num_matrices; i++)
 	{
@@ -95,8 +100,7 @@ allocateMemory (size_t size)
 
 	if ((p = malloc(size)) == NULL)
 	{
-		printf("\n\nSpeicherprobleme!\n");
-		/* exit program */
+		printf("Speicherprobleme! (%" PRIu64 " Bytes angefordert)\n", size);
 		exit(1);
 	}
 
@@ -110,9 +114,9 @@ static
 void
 allocateMatrices (struct calculation_arguments* arguments)
 {
-	int i, j;
+	uint64_t i, j;
 
-	int N = arguments->N;
+	uint64_t const N = arguments->N;
 
 	arguments->M = allocateMemory(arguments->num_matrices * (N + 1) * (N + 1) * sizeof(double));
 	arguments->Matrix = allocateMemory(arguments->num_matrices * sizeof(double**));
@@ -123,7 +127,7 @@ allocateMatrices (struct calculation_arguments* arguments)
 
 		for (j = 0; j <= N; j++)
 		{
-			arguments->Matrix[i][j] = (double*)(arguments->M + (i * (N + 1) * (N + 1)) + (j * (N + 1)));
+			arguments->Matrix[i][j] = arguments->M + (i * (N + 1) * (N + 1)) + (j * (N + 1));
 		}
 	}
 }
@@ -133,12 +137,12 @@ allocateMatrices (struct calculation_arguments* arguments)
 /* ************************************************************************ */
 static
 void
-initMatrices (struct calculation_arguments* arguments, struct options* options)
+initMatrices (struct calculation_arguments* arguments, struct options const* options)
 {
-	int g, i, j;                                /*  local variables for loops   */
+	uint64_t g, i, j;                                /*  local variables for loops   */
 
-	int N = arguments->N;
-	double h = arguments->h;
+	uint64_t const N = arguments->N;
+	double const h = arguments->h;
 	double*** Matrix = arguments->Matrix;
 
 	/* initialize matrix/matrices with zeros */
@@ -148,7 +152,7 @@ initMatrices (struct calculation_arguments* arguments, struct options* options)
 		{
 			for (j = 0; j <= N; j++)
 			{
-				Matrix[g][i][j] = 0;
+				Matrix[g][i][j] = 0.0;
 			}
 		}
 	}
@@ -156,21 +160,18 @@ initMatrices (struct calculation_arguments* arguments, struct options* options)
 	/* initialize borders, depending on function (function 2: nothing to do) */
 	if (options->inf_func == FUNC_F0)
 	{
-		for(i = 0; i <= N; i++)
+		for (g = 0; g < arguments->num_matrices; g++)
 		{
-			for (j = 0; j < arguments->num_matrices; j++)
+			for (i = 0; i <= N; i++)
 			{
-				Matrix[j][i][0] = 1 - (h * i);
-				Matrix[j][i][N] = h * i;
-				Matrix[j][0][i] = 1 - (h * i);
-				Matrix[j][N][i] = h * i;
+				Matrix[g][i][0] = 1.0 - (h * i);
+				Matrix[g][i][N] = h * i;
+				Matrix[g][0][i] = 1.0 - (h * i);
+				Matrix[g][N][i] = h * i;
 			}
-		}
 
-		for (j = 0; j < arguments->num_matrices; j++)
-		{
-			Matrix[j][N][0] = 0;
-			Matrix[j][0][N] = 0;
+			Matrix[g][N][0] = 0.0;
+			Matrix[g][0][N] = 0.0;
 		}
 	}
 }
@@ -180,46 +181,75 @@ initMatrices (struct calculation_arguments* arguments, struct options* options)
 /* ************************************************************************ */
 static
 void
-calculate (struct calculation_arguments* arguments, struct calculation_results *results, struct options* options)
+calculate (struct calculation_arguments const* arguments, struct calculation_results* results, struct options const* options)
 {
-	int i, j;                                   /* local variables for loops  */
-	int m1, m2;                                 /* used as indices for old and new matrices       */
+	int i, j;                                   /* local variables for loops */
+	int m1, m2;                                 /* used as indices for old and new matrices */
 	double star;                                /* four times center value minus 4 neigh.b values */
-	double korrektur;
-	double residuum;                            /* residuum of current iteration                  */
+	double residuum;                            /* residuum of current iteration */
 	double maxresiduum;                         /* maximum residuum value of a slave in iteration */
 
-	int N = arguments->N;
-	double*** Matrix = arguments->Matrix;
+	int const N = arguments->N;
+	double const h = arguments->h;
+
+	double pih = 0.0;
+	double fpisin = 0.0;
+
+	int term_iteration = options->term_iteration;
 
 	/* initialize m1 and m2 depending on algorithm */
-	if (options->method == METH_GAUSS_SEIDEL)
+	if (options->method == METH_JACOBI)
 	{
-		m1=0; m2=0;
+		m1 = 0;
+		m2 = 1;
 	}
 	else
 	{
-		m1=0; m2=1;
+		m1 = 0;
+		m2 = 0;
 	}
 
-	while (options->term_iteration > 0)
+	if (options->inf_func == FUNC_FPISIN)
 	{
+		pih = PI * h;
+		fpisin = 0.25 * TWO_PI_SQUARE * h * h;
+	}
+
+	while (term_iteration > 0)
+	{
+		double** Matrix_Out = arguments->Matrix[m1];
+		double** Matrix_In  = arguments->Matrix[m2];
+
 		maxresiduum = 0;
 
 		/* over all rows */
 		for (i = 1; i < N; i++)
 		{
+			double fpisin_i = 0.0;
+
+			if (options->inf_func == FUNC_FPISIN)
+			{
+				fpisin_i = fpisin * sin(pih * (double)i);
+			}
+
 			/* over all columns */
 			for (j = 1; j < N; j++)
 			{
-				star = -Matrix[m2][i-1][j] - Matrix[m2][i][j-1] - Matrix[m2][i][j+1] - Matrix[m2][i+1][j] + 4.0 * Matrix[m2][i][j];
+				star = 0.25 * (Matrix_In[i-1][j] + Matrix_In[i][j-1] + Matrix_In[i][j+1] + Matrix_In[i+1][j]);
 
-				residuum = star / 4.0;
-				korrektur = residuum;
-				residuum = (residuum < 0) ? -residuum : residuum;
-				maxresiduum = (residuum < maxresiduum) ? maxresiduum : residuum;
+				if (options->inf_func == FUNC_FPISIN)
+				{
+					star += fpisin_i * sin(pih * (double)j);
+				}
 
-				Matrix[m1][i][j] = Matrix[m2][i][j] - korrektur;
+				if (options->termination == TERM_PREC || term_iteration == 1)
+				{
+					residuum = Matrix_In[i][j] - star;
+					residuum = (residuum < 0) ? -residuum : residuum;
+					maxresiduum = (residuum < maxresiduum) ? maxresiduum : residuum;
+				}
+
+				Matrix_Out[i][j] = star;
 			}
 		}
 
@@ -227,19 +257,21 @@ calculate (struct calculation_arguments* arguments, struct calculation_results *
 		results->stat_precision = maxresiduum;
 
 		/* exchange m1 and m2 */
-		i=m1; m1=m2; m2=i;
+		i = m1;
+		m1 = m2;
+		m2 = i;
 
-		/* check for stopping calculation, depending on termination method */
+		/* check for stopping calculation depending on termination method */
 		if (options->termination == TERM_PREC)
 		{
 			if (maxresiduum < options->term_precision)
 			{
-				options->term_iteration = 0;
+				term_iteration = 0;
 			}
 		}
 		else if (options->termination == TERM_ITER)
 		{
-			options->term_iteration--;
+			term_iteration--;
 		}
 	}
 
@@ -251,18 +283,18 @@ calculate (struct calculation_arguments* arguments, struct calculation_results *
 /* ************************************************************************ */
 static
 void
-displayStatistics (struct calculation_arguments* arguments, struct calculation_results *results, struct options* options)
+displayStatistics (struct calculation_arguments const* arguments, struct calculation_results const* results, struct options const* options)
 {
-	(void)arguments;
-
+	int N = arguments->N;
 	double time = (comp_time.tv_sec - start_time.tv_sec) + (comp_time.tv_usec - start_time.tv_usec) * 1e-6;
-	printf("Berechnungszeit:    %f s \n", time);
 
+	printf("Berechnungszeit:    %f s \n", time);
+	printf("Speicherbedarf:     %f MiB\n", (N + 1) * (N + 1) * sizeof(double) * arguments->num_matrices / 1024.0 / 1024.0);
 	printf("Berechnungsmethode: ");
 
 	if (options->method == METH_GAUSS_SEIDEL)
 	{
-		printf("Gauss-Seidel");
+		printf("Gauß-Seidel");
 	}
 	else if (options->method == METH_JACOBI)
 	{
@@ -270,16 +302,16 @@ displayStatistics (struct calculation_arguments* arguments, struct calculation_r
 	}
 
 	printf("\n");
-	printf("Interlines:         %d\n",options->interlines);
+	printf("Interlines:         %" PRIu64 "\n",options->interlines);
 	printf("Stoerfunktion:      ");
 
 	if (options->inf_func == FUNC_F0)
 	{
-		printf("f(x,y)=0");
+		printf("f(x,y) = 0");
 	}
 	else if (options->inf_func == FUNC_FPISIN)
 	{
-		printf("f(x,y)=2pi^2*sin(pi*x)sin(pi*y)");
+		printf("f(x,y) = 2pi^2*sin(pi*x)sin(pi*y)");
 	}
 
 	printf("\n");
@@ -295,8 +327,44 @@ displayStatistics (struct calculation_arguments* arguments, struct calculation_r
 	}
 
 	printf("\n");
-	printf("Anzahl Iterationen: %d\n", results->stat_iteration);
+	printf("Anzahl Iterationen: %" PRIu64 "\n", results->stat_iteration);
 	printf("Norm des Fehlers:   %e\n", results->stat_precision);
+	printf("\n");
+}
+
+/****************************************************************************/
+/** Beschreibung der Funktion displayMatrix:                               **/
+/**                                                                        **/
+/** Die Funktion displayMatrix gibt eine Matrix                            **/
+/** in einer "ubersichtlichen Art und Weise auf die Standardausgabe aus.   **/
+/**                                                                        **/
+/** Die "Ubersichtlichkeit wird erreicht, indem nur ein Teil der Matrix    **/
+/** ausgegeben wird. Aus der Matrix werden die Randzeilen/-spalten sowie   **/
+/** sieben Zwischenzeilen ausgegeben.                                      **/
+/****************************************************************************/
+static
+void
+displayMatrix (struct calculation_arguments* arguments, struct calculation_results* results, struct options* options)
+{
+	int x, y;
+
+	double** Matrix = arguments->Matrix[results->m];
+
+	int const interlines = options->interlines;
+
+	printf("Matrix:\n");
+
+	for (y = 0; y < 9; y++)
+	{
+		for (x = 0; x < 9; x++)
+		{
+			printf ("%7.4f", Matrix[y * (interlines + 1)][x * (interlines + 1)]);
+		}
+
+		printf ("\n");
+	}
+
+	fflush (stdout);
 }
 
 /* ************************************************************************ */
@@ -309,23 +377,21 @@ main (int argc, char** argv)
 	struct calculation_arguments arguments;
 	struct calculation_results results;
 
-	/* get parameters */
-	AskParams(&options, argc, argv);              /* ************************* */
+	askParams(&options, argc, argv);
 
-	initVariables(&arguments, &results, &options);           /* ******************************************* */
+	initVariables(&arguments, &results, &options);
 
-	allocateMatrices(&arguments);        /*  get and initialize variables and matrices  */
-	initMatrices(&arguments, &options);            /* ******************************************* */
+	allocateMatrices(&arguments);
+	initMatrices(&arguments, &options);
 
-	gettimeofday(&start_time, NULL);                   /*  start timer         */
-	calculate(&arguments, &results, &options);                                      /*  solve the equation  */
-	gettimeofday(&comp_time, NULL);                   /*  stop timer          */
+	gettimeofday(&start_time, NULL);
+	calculate(&arguments, &results, &options);
+	gettimeofday(&comp_time, NULL);
 
-	displayStatistics(&arguments, &results, &options);                                  /* **************** */
-	DisplayMatrix("Matrix:",                              /*  display some    */
-			arguments.Matrix[results.m][0], options.interlines);            /*  statistics and  */
+	displayStatistics(&arguments, &results, &options);
+	displayMatrix(&arguments, &results, &options);
 
-	freeMatrices(&arguments);                                       /*  free memory     */
+	freeMatrices(&arguments);
 
 	return 0;
 }
