f4d834efc63941df085a26326226b0635f5afd97
Yannik Könneker <y.koenneker@web.de>
2019-11-07 19:32:42 +0100

    small update

--- 04-PDE/partdiff.c
+++ 04-PDE/partdiff.c
@@ -222,7 +222,7 @@ calculate (struct calculation_arguments const* arguments, struct calculation_res
 
 		maxresiduum = 0;
 		
-		#pragma omp parallel guided for schedule(dynamic) private(fpisin_i, i, j, star, residuum)
+		#pragma omp parallel for guided schedule(dynamic) private(fpisin_i, i, j, star, residuum)
 		/* over all rows */
 		for (i = 1; i < N; i++)
 		{
