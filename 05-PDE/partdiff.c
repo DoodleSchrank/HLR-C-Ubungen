968be9a409833a59c0c2ebd3f37e5d6c4ea0b8ae
Yannik Könneker <y.koenneker@web.de>
2019-11-14 10:33:33 +0100

    small

--- 05-PDE/partdiff.c
+++ 05-PDE/partdiff.c
@@ -222,7 +222,7 @@ static double *calculaterow(struct pthread_parameters *param)
 			*param->Matrix_Out[i][j] = star;
 		}
 	}
-	return maxresiduum;
+	return *maxresiduum;
 }
 
 
@@ -293,7 +293,7 @@ calculate (struct calculation_arguments const* arguments, struct calculation_res
 		// Gabel
 		for(i = 1; i < options->number; i++)
 		{
-			pthread_create(&threads[i-1], NULL, calculaterow, params[i]);
+			pthread_create(&threads[i-1], NULL, *calculaterow, params[i]);
 		}
 		
 		// No part-timers!
