a18d8f0e9d001dd3767bfa1ad894d52de2731564
Yannik Koenneker <koenneker@wr.informatik.uni-hamburg.de>
2019-11-21 00:02:51 +0100

    made something, debugging sux :(

--- 06-MPI/timempi.c
+++ 06-MPI/timempi.c
@@ -3,36 +3,18 @@
 #include <unistd.h>
 #include <sys/time.h>
 
-int main(void)
-{
-	MPI_Init();
-	int myid, numThreads;
-	MPI_Datatype datastruct;
-	
-	MPI_Comm_rank(MPI_COMM_WORLD, &myid);
-	MPI_Comm_size(MPI_COMM_WORLD, &numThreads);
-	MPI_Type_struct(2, {1, 1} {0, 1}, {MPI_CHAR, MPI_LONG}, &datastruct);
-	MPI_Type_commit(&datastruct);
-	
-	if( == 0) master(numThreads);
-	else slave();
-	
-	MPI_Finalize();
-	return 0;
-}
-
-void master(int numThreads)
+void meister(int numThreads)
 {
 	MPI_Datatype datastruct;
 	for(int i = 1; i < numThreads; i++)
 	{
-		MPI_Recv(&datastruct, 1, MPI_Type_struct, i, void , void , void);
+		MPI_Recv(&datastruct, 1, datastruct, i, void , void , void);
 		printf("%s:%ld\n", datastruct.hostname, datastruct.time);
 	}
 	
 }
 
-void slave()
+void sklave()
 {
 	MPI_Datatype datastruct;
 	char hostname[256];
@@ -40,7 +22,7 @@ void slave()
 	struct timezone tz;
 	
 	if(gethostname(hostname, sizeof(hostname)) != 0) hostname = "Error";
-	if(gettimeofday(*tv, *tz) != 0) tv.tv_usec = 0;
+	if(gettimeofday(&tv, &tz) != 0) tv.tv_usec = 0;
 	
 	struct data
 	{
@@ -48,4 +30,26 @@ void slave()
 		long int time = tv.tv_usec;
 	} data;
 	MPI_Send(&data, 1, datastruct, 0, void, void);
+}
+
+int main(int argc, char* argv[])
+{
+	MPI_Init(&argc, &argv);
+	int myid, numThreads;
+	MPI_Datatype datastruct;
+	
+	MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+	MPI_Comm_size(MPI_COMM_WORLD, &numThreads);
+	int blocklength[] = {1, 1};
+	int displacements[] = {0, 1};
+	MPI_Datatype types[] = {MPI_CHAR, MPI_LONG};
+
+	MPI_Type_struct(2, blocklength, displacements, types, &datastruct);
+	MPI_Type_commit(&datastruct);
+	
+	if(numThreads == 0) meister(numThreads);
+	else sklave();
+	
+	MPI_Finalize();
+	return 0;
 }
