29afabe3d484e3cf941556f579a8dd148fb106d8
Yannik Könneker <y.koenneker@web.de>
2019-11-20 23:48:00 +0100

    Begin 06

---
+++ 06-MPI/timempi.c
@@ -0,0 +1,51 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/time.h>
+
+int main(void)
+{
+	MPI_Init();
+	int myid, numThreads;
+	MPI_Datatype datastruct;
+	
+	MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+	MPI_Comm_size(MPI_COMM_WORLD, &numThreads);
+	MPI_Type_struct(2, {1, 1} {0, 1}, {MPI_CHAR, MPI_LONG}, &datastruct);
+	MPI_Type_commit(&datastruct);
+	
+	if( == 0) master(numThreads);
+	else slave();
+	
+	MPI_Finalize();
+	return 0;
+}
+
+void master(int numThreads)
+{
+	MPI_Datatype datastruct;
+	for(int i = 1; i < numThreads; i++)
+	{
+		MPI_Recv(&datastruct, 1, MPI_Type_struct, i, void , void , void);
+		printf("%s:%ld\n", datastruct.hostname, datastruct.time);
+	}
+	
+}
+
+void slave()
+{
+	MPI_Datatype datastruct;
+	char hostname[256];
+	struct timeval tv;
+	struct timezone tz;
+	
+	if(gethostname(hostname, sizeof(hostname)) != 0) hostname = "Error";
+	if(gettimeofday(*tv, *tz) != 0) tv.tv_usec = 0;
+	
+	struct data
+	{
+		char *hostname = hostname;
+		long int time = tv.tv_usec;
+	} data;
+	MPI_Send(&data, 1, datastruct, 0, void, void);
+}
