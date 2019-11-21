8665fc3eecfddb0cdfb7c7ef3b072c0e8f4f00d1
Yannik Koenneker <koenneker@wr.informatik.uni-hamburg.de>
2019-11-21 21:43:01 +0100

    fixed some problemes, created new ones :( currently gethostname() doesnt work as indented

--- 06-MPI/timempi.c
+++ 06-MPI/timempi.c
@@ -1,37 +1,36 @@
 #include <mpi.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/time.h>
+#include <string.h>
+#include <stdlib.h> 
+
 
 void meister(int numThreads)
 {
-	MPI_Datatype datastruct;
-	MPI_Status status;
-	int tag;
+	char data[256];
 	for(int i = 1; i < numThreads; i++)
 	{
-		MPI_Recv(&datastruct, 1, datastruct, i, tag , MPI_COMM_WORLD , &status);
-		printf("%s:%ld\n", datastruct.hostname, datastruct.time);
+		MPI_Recv(&data, 256, MPI_CHAR, i, 0, MPI_COMM_WORLD , MPI_STATUS_IGNORE);
+		printf("%s\n", data);
 	}	
 }
 
 void sklave()
 {
-	MPI_Datatype datastruct;
-	char hostname[256];
 	struct timeval tv;
-	struct timezone tz;
-	int tag;
+	char data[256], time[256], hostname[HOST_NAME_MAX];
+
+	if(gethostname(hostname, sizeof(hostname)) != 0) strcat(hostname, "Error");
+	if(gettimeofday(&tv, NULL) != 0) tv.tv_usec = 0;
 	
-	if(gethostname(hostname, sizeof(hostname)) != 0) hostname = "Error";
-	if(gettimeofday(&tv, &tz) != 0) tv.tv_usec = 0;
-	
-	struct data
-	{
-		char *host = hostname;
-		long int time = tv.tv_usec;
-	} data;
-	MPI_Send(&data, 1, datastruct, 0, tag, MPI_COMM_WORLD);
+	strcat(hostname, ": ");
+	sprintf(time, "%ld", tv.tv_sec);
+	strcat(data, hostname);
+	strcat(data, time);
+
+	MPI_Send(&data, 256, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
 }
 
 int main(int argc, char* argv[])
@@ -42,20 +41,14 @@ int main(int argc, char* argv[])
 	MPI_Comm_rank(MPI_COMM_WORLD, &myid);
 	MPI_Comm_size(MPI_COMM_WORLD, &numThreads);
 
-	// define MPI_struct
-	static MPI_Datatype datastruct;
-	int blocklength[] = {256, 1};
-	long int displacements[] = {0, sizeof(char[256]};
-	MPI_Datatype types[] = {MPI_CHAR, MPI_LONG};
-
-	MPI_Type_struct(2, blocklength, displacements, types, &datastruct);
-	MPI_Type_commit(&datastruct);
-	
-
-	if(myid == 0) meister(numThreads);
-	else sklave();
+	if(myid != 0) meister(numThreads);
+	else sklave(myid);
 	
 	MPI_Finalize();
-	printf("All those moments will be lost in time, like tears in rain. Time to die. (%s)", ((myid == 0) ? "Meister" : "Sklave %d", myid));
+	
+	char string[256] = "";
+	if (myid == 0) snprintf(string, sizeof(string), "Meister %d", myid);
+	else snprintf(string, sizeof(string),  "Sklave %d", myid);
+	printf("All those moments will be lost in time, like tears in rain. Time to die. (%s)\n", string);
 	return 0;
 }
