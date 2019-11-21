21e168bcedaf40b58538ff21a70ba44f815eeafa
Yannik Koenneker <koenneker@wr.informatik.uni-hamburg.de>
2019-11-21 12:53:49 +0100

    fix most errors, still have to repair mpi_struct

--- 06-MPI/timempi.c
+++ 06-MPI/timempi.c
@@ -6,12 +6,13 @@
 void meister(int numThreads)
 {
 	MPI_Datatype datastruct;
+	MPI_Status status;
+	int tag;
 	for(int i = 1; i < numThreads; i++)
 	{
-		MPI_Recv(&datastruct, 1, datastruct, i, void , void , void);
+		MPI_Recv(&datastruct, 1, datastruct, i, tag , MPI_COMM_WORLD , &status);
 		printf("%s:%ld\n", datastruct.hostname, datastruct.time);
-	}
-	
+	}	
 }
 
 void sklave()
@@ -20,36 +21,41 @@ void sklave()
 	char hostname[256];
 	struct timeval tv;
 	struct timezone tz;
+	int tag;
 	
 	if(gethostname(hostname, sizeof(hostname)) != 0) hostname = "Error";
 	if(gettimeofday(&tv, &tz) != 0) tv.tv_usec = 0;
 	
 	struct data
 	{
-		char *hostname = hostname;
+		char host = hostname;
 		long int time = tv.tv_usec;
 	} data;
-	MPI_Send(&data, 1, datastruct, 0, void, void);
+	MPI_Send(&data, 1, datastruct, 0, tag, MPI_COMM_WORLD);
 }
 
 int main(int argc, char* argv[])
 {
 	MPI_Init(&argc, &argv);
 	int myid, numThreads;
-	MPI_Datatype datastruct;
 	
 	MPI_Comm_rank(MPI_COMM_WORLD, &myid);
 	MPI_Comm_size(MPI_COMM_WORLD, &numThreads);
+
+	// define MPI_struct
+	static MPI_Datatype datastruct;
 	int blocklength[] = {1, 1};
-	int displacements[] = {0, 1};
+	long int displacements[] = {0, 1};
 	MPI_Datatype types[] = {MPI_CHAR, MPI_LONG};
 
 	MPI_Type_struct(2, blocklength, displacements, types, &datastruct);
 	MPI_Type_commit(&datastruct);
 	
-	if(numThreads == 0) meister(numThreads);
+
+	if(myid == 0) meister(numThreads);
 	else sklave();
 	
 	MPI_Finalize();
+	printf("All those moments will be lost in time, like tears in rain. Time to die. (%s)", ((myid == 0) ? "Meister" : "Sklave %d", myid));
 	return 0;
 }
