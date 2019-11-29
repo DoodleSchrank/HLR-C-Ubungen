5601551a86e7730ed9143a3e928d6b7aa691a9ed
Yannik Koenneker <koenneker@wr.informatik.uni-hamburg.de>
2019-11-29 10:12:07 +0100

    works now, next time, write MPI data to the address, not the address itself

--- 07-circle/circle.c
+++ 07-circle/circle.c
@@ -18,13 +18,13 @@ init (void)
 		int i, j;
 		srand(time(NULL));
 		
-		int* sendbuf = malloc(sizeof(int) * length);
+		int sendbuf[length]; 
 		for (i = 0; i < numThreads; i++)
 		{
 			for (j = 0; j < length; j++)
 			{
 				//wenn Reste vorhanden, soll dieser auf die ersten x Threads verteilt werden, nachfolgende Threads haben im letzten Arrayeintrag 'NULL'
-				if(i >= numThreads - rest && j == length - 1)
+				if(i >= numThreads - (numThreads - rest)  && j == length - 1)
 					sendbuf[j] = -1;
 				else
 				{
@@ -35,7 +35,7 @@ init (void)
 
 			//Send data, or save, if you are Zero
 			if(i != 0)
-				MPI_Send(&sendbuf, length, MPI_INT, i, 0, MPI_COMM_WORLD);
+				MPI_Send(sendbuf, length, MPI_INT, i, 0, MPI_COMM_WORLD);
 			else
 			{
 				for(j = 0; j < length; j++)
@@ -45,12 +45,11 @@ init (void)
 
 		//Send first int to last Thread
 		MPI_Send(&buf[0], 1, MPI_INT, numThreads - 1, 0, MPI_COMM_WORLD);
-		free(sendbuf);
 	}
 	else
 	{
 		//Receive your part of the pie :)
-		MPI_Recv(&buf, length, MPI_INT, 0, 0, MPI_COMM_WORLD , MPI_STATUS_IGNORE);
+		MPI_Recv(buf, length, MPI_INT, 0, 0, MPI_COMM_WORLD , MPI_STATUS_IGNORE);
 	}
 
 	//Last Thread receives the first int.
@@ -60,41 +59,40 @@ init (void)
 	return buf;
 }
 
-int
-*circle (int* buf)
+void
+circle (int* buf)
 {
-	int *recvbuf = malloc(sizeof(int) * length);
+	MPI_Status stat;
+	int sendbuf[length];
 	int done = 0;
 	int target = ((rank + 1) % numThreads);
-	int source = ((rank - 1) % numThreads);
+	int source = (((rank - 1) % numThreads) + numThreads) % numThreads;
 	MPI_Request req;
 	int i;
 
 	while(done == 0)
 	{
+		for(i = 0; i < length; i++)
+			sendbuf[i] = buf[i];
 		//send data asynchronisly, for immediate receive
-		MPI_Isend(&buf, length, MPI_INT, target, 0, MPI_COMM_WORLD, &req);
-		MPI_Recv(&recvbuf, length, MPI_INT, source, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		MPI_Isend(sendbuf, length, MPI_INT, target, 0, MPI_COMM_WORLD, &req);
+		MPI_Recv(buf, length, MPI_INT, source, 0, MPI_COMM_WORLD, &stat);
 		
 		//wait for sent data and write recvbuf to buf
-		MPI_Wait(&req, MPI_STATUS_IGNORE);
-		for(i = 0; i < length; i++)
-			buf[i] = recvbuf[i];
+		MPI_Wait(&req, &stat);
 
 		//if we are done, broadcast so.
 		if(rank == numThreads - 1 && buf[0] == first)
 			done = 1;
 		MPI_Bcast(&done, 1, MPI_INT, numThreads - 1, MPI_COMM_WORLD);
 	}
-	free(recvbuf);
-	return buf;
 }
 
 int
 main (int argc, char** argv)
 {
 	MPI_Init(&argc, &argv);
-	int N, i;
+	int N, i, j;
 	int *buf;
 
 	if (argc < 2)
@@ -116,21 +114,35 @@ main (int argc, char** argv)
 	//some printy boys
 	if(rank == 0)
 		printf("\nBEFORE\n");
-	for (i = 0; i < length; i++)
-	{
-		if(buf[i] > -1)
-			printf("rank %d: %d\n", rank, buf[i]);
+	for (i = 0; i < numThreads; i++)
+	{	
+		if (i == rank)
+		{
+			for (j = 0; j < length; j++)
+			{
+				if(buf[j] > -1)
+					printf("rank %d: %d\n", rank, buf[j]);
+			}
+		}
+		MPI_Barrier(MPI_COMM_WORLD);
 	}
 
-	buf = circle(buf);
+	circle(buf);
 	
 	//more printy boys :o
 	if(rank == 0)
 		printf("\nAFTER\n");
-	for (i = 0; i < length; i++)
-	{
-		if(buf[i] > -1)
-			printf("rank %d: %d\n", rank, buf[i]);
+	for (i = 0; i < numThreads; i++)
+	{	
+		if (i == rank)
+		{
+			for (j = 0; j < length; j++)
+			{
+				if(buf[j] > -1)
+					printf("rank %d: %d\n", rank, buf[j]);
+			}
+		}
+		MPI_Barrier(MPI_COMM_WORLD);
 	}
 
 	free(buf);
