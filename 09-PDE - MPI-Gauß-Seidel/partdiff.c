e94fbe983779784e105d8b67ddce22c5d002c02e
Yannik Koenneker <koenneker@wr.informatik.uni-hamburg.de>
2020-01-05 20:00:59 +0100

    fixes

--- 09-PDE - MPI-Gauß-Seidel/partdiff.c
+++ 09-PDE - MPI-Gauß-Seidel/partdiff.c
@@ -1,23 +1,3 @@
-/****************************************************************************/
-/****************************************************************************/
-/**																		**/
-/**				 TU München - Institut für Informatik				   **/
-/**																		**/
-/** Copyright: Prof. Dr. Thomas Ludwig									 **/
-/**			Andreas C. Schmidt										  **/
-/**																		**/
-/** File:	  partdiff.c												  **/
-/**																		**/
-/** Purpose:   Partial differential equation solver for Gauß-Seidel and	**/
-/**			Jacobi method.											  **/
-/**																		**/
-/****************************************************************************/
-/****************************************************************************/
-
-/* ************************************************************************ */
-/* Include standard header file.											*/
-/* ************************************************************************ */
-#define _POSIX_C_SOURCE 200809 L
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -66,15 +46,15 @@ static
 void
 initVariables(struct calculation_arguments * arguments, struct calculation_results * results, struct options
 	const * options) {
-	arguments - > N = (options - > interlines * 8) + 9 - 1;
-	arguments - > num_matrices = (options - > method == METH_JACOBI) ? 2 : 1;
-	arguments - > h = 1.0 / arguments - > N;
+	arguments -> N = (options -> interlines * 8) + 9 - 1;
+	arguments -> num_matrices = (options -> method == METH_JACOBI) ? 2 : 1;
+	arguments -> h = 1.0 / arguments -> N;
 
-	results - > m = 0;
-	results - > stat_iteration = 0;
-	results - > stat_precision = 0;
+	results -> m = 0;
+	results -> stat_iteration = 0;
+	results -> stat_precision = 0;
 
-	uint64_t N = arguments - > N;
+	uint64_t N = arguments -> N;
 
 	// safety if interlines > numThreads / 4 
 	// division by 4 to guarantee at least some benefit from paralellization
@@ -96,12 +76,12 @@ void
 freeMatrices(struct calculation_arguments * arguments) {
 	uint64_t i;
 
-	for (i = 0; i < arguments - > num_matrices; i++) {
-		free(arguments - > Matrix[i]);
+	for (i = 0; i < arguments -> num_matrices; i++) {
+		free(arguments -> Matrix[i]);
 	}
 
-	free(arguments - > Matrix);
-	free(arguments - > M);
+	free(arguments -> Matrix);
+	free(arguments -> M);
 }
 
 /* ************************************************************************ */
@@ -131,18 +111,18 @@ allocateMatrices(struct calculation_arguments * arguments) {
 	uint64_t i, j;
 
 	uint64_t
-	const N = arguments - > N;
+	const N = arguments -> N;
 	uint64_t
 	const size = matrix_size + 2;
 
-	arguments - > M = allocateMemory(arguments - > num_matrices * (N + 1) * size * sizeof(double));
-	arguments - > Matrix = allocateMemory(arguments - > num_matrices * sizeof(double ** ));
+	arguments -> M = allocateMemory(arguments -> num_matrices * (N + 1) * size * sizeof(double));
+	arguments -> Matrix = allocateMemory(arguments -> num_matrices * sizeof(double ** ));
 
-	for (i = 0; i < arguments - > num_matrices; i++) {
-		arguments - > Matrix[i] = allocateMemory((N + 1) * sizeof(double * ));
+	for (i = 0; i < arguments -> num_matrices; i++) {
+		arguments -> Matrix[i] = allocateMemory((N + 1) * sizeof(double * ));
 
 		for (j = 0; j <= N; j++) {
-			arguments - > Matrix[i][j] = arguments - > M + (i * (N + 1) * size) + (j * (N + 1));
+			arguments -> Matrix[i][j] = arguments -> M + (i * (N + 1) * size) + (j * (N + 1));
 		}
 	}
 }
@@ -158,16 +138,16 @@ initMatrices(struct calculation_arguments * arguments, struct options
 	uint64_t g, i, j;
 
 	uint64_t
-	const N = arguments - > N;
+	const N = arguments -> N;
 	uint64_t
 	const size = matrix_size + 2;
 
 	double
-	const h = arguments - > h;
-	double ** * Matrix = arguments - > Matrix;
+	const h = arguments -> h;
+	double ** * Matrix = arguments -> Matrix;
 
 	// initialize matrix/matrices with zeros
-	for (g = 0; g < arguments - > num_matrices; g++) {
+	for (g = 0; g < arguments -> num_matrices; g++) {
 		for (i = 0; i < size; i++) {
 			for (j = 0; j < N; j++) {
 				Matrix[g][i][j] = 0.0;
@@ -176,8 +156,8 @@ initMatrices(struct calculation_arguments * arguments, struct options
 	}
 
 	// initialize borders, depending on function (function 2: nothing to do)
-	if (options - > inf_func == FUNC_F0) {
-		for (g = 0; g < arguments - > num_matrices; g++) {
+	if (options -> inf_func == FUNC_F0) {
+		for (g = 0; g < arguments -> num_matrices; g++) {
 			for (i = 0; i < size; i++) {
 				Matrix[g][i][0] = 1.0 - (h * (i + matrix_from - 1));
 				Matrix[g][i][N] = h * (i + matrix_from - 1);
@@ -213,9 +193,9 @@ calculate(struct calculation_arguments
 	int m1, m2;
 
 	uint64_t
-	const N = arguments - > N;
+	const N = arguments -> N;
 	double
-	const h = arguments - > h;
+	const h = arguments -> h;
 
 	double pih = 0.0;
 	double fpisin = 0.0;
@@ -230,10 +210,10 @@ calculate(struct calculation_arguments
 	double fpisin_i = 0.0;
 	double maxresidaa[numThreads];
 
-	int term_iteration = options - > term_iteration;
+	int term_iteration = options -> term_iteration;
 
 	/* initialize m1 and m2 depending on algorithm */
-	if (options - > method == METH_JACOBI) {
+	if (options -> method == METH_JACOBI) {
 		m1 = 0;
 		m2 = 1;
 	} else {
@@ -241,36 +221,36 @@ calculate(struct calculation_arguments
 		m2 = 0;
 	}
 
-	if (options - > inf_func == FUNC_FPISIN) {
+	if (options -> inf_func == FUNC_FPISIN) {
 		pih = PI * h;
 		fpisin = 0.25 * TWO_PI_SQUARE * h * h;
 	}
 
 	//rank 0 prepares for receiving maxresidaä of other processes
-	if (rank == 0 && options - > termination == TERM_PREC) {
+	if (rank == 0 && options -> termination == TERM_PREC) {
 		if (numThreads > 1) {
 			for (i = 1; i < numThreads; i++)
 				MPI_Irecv( & maxresidaa[i], 1, MPI_DOUBLE, i, 1, MPI_COMM_WORLD, & reqRes);
 		}
 	}
 	//the others prepare for receiving maxres (the one that cancels this whole operation)
-	else if (options - > termination == TERM_PREC)
+	else if (options -> termination == TERM_PREC)
 		MPI_Irecv( & maxres, 1, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD, & reqRes);
 
 	// Recieve first row if Gauß-Seidel, needs to be done before lööp
-	if (rank != 0 && options - > method == METH_GAUSS_SEIDEL)
+	if (rank != 0 && options -> method == METH_GAUSS_SEIDEL)
 		MPI_Irecv(Matrix_In[0], N + 1, MPI_DOUBLE, source, 0, MPI_COMM_WORLD, & reqRecvFirst);
 
 	while (term_iteration > 0) {
-		Matrix_Out = arguments - > Matrix[m1];
-		Matrix_In = arguments - > Matrix[m2];
+		Matrix_Out = arguments -> Matrix[m1];
+		Matrix_In = arguments -> Matrix[m2];
 
 		maxresiduum = 0;
 
 		omp_set_dynamic(0);
-		#pragma omp parallel for private(j, star, fpisin_i, residuum) reduction(max: maxresiduum) num_threads(options - > number)
+		#pragma omp parallel for private(j, star, fpisin_i, residuum) reduction(max: maxresiduum) num_threads(options -> number)
 		for (i = 1; i < N; i++) {
-			if (options - > inf_func == FUNC_FPISIN)
+			if (options -> inf_func == FUNC_FPISIN)
 				fpisin_i = fpisin * sin(pih * (double) i);
 
 			// Wait for first row to be recieved
@@ -285,7 +265,7 @@ calculate(struct calculation_arguments
 			}
 
 			// Wait for last row to be sent
-			if (results - > stat_iteration > 0 && rank < numThreads - 1 && i == matrix_size) {
+			if (results -> stat_iteration > 0 && rank < numThreads - 1 && i == matrix_size) {
 				MPI_Wait( & reqSendLast, MPI_STATUS_IGNORE);
 				MPI_Wait( & reqRecvLast, MPI_STATUS_IGNORE);
 			}
@@ -293,10 +273,10 @@ calculate(struct calculation_arguments
 			for (j = 1; j < N; j++) {
 				star = 0.25 * (Matrix_In[i - 1][j] + Matrix_In[i][j - 1] + Matrix_In[i][j + 1] + Matrix_In[i + 1][j]);
 
-				if (options - > inf_func == FUNC_FPISIN)
+				if (options -> inf_func == FUNC_FPISIN)
 					star += fpisin_i * sin(pih * (double) j);
 
-				if (options - > termination == TERM_PREC || term_iteration == 1) {
+				if (options -> termination == TERM_PREC || term_iteration == 1) {
 					residuum = Matrix_In[i][j] - star;
 					residuum = (residuum < 0) ? -residuum : residuum;
 					maxresiduum = (residuum < maxresiduum) ? maxresiduum : residuum;
@@ -318,29 +298,29 @@ calculate(struct calculation_arguments
 		m2 = i;
 
 		/* check for stopping calculation depending on termination method */
-		if (options - > termination == TERM_PREC) {
+		if (options -> termination == TERM_PREC) {
 			//Reduce Maxresiduum and, if criteria is met, send maxres to other processes to kill them (after some time)
 			if (rank == 0) {
 				maxresidaa[0] = maxresiduum;
 				maxres = 0.0;
 				for (i = 0; i < numThreads; i++)
 					maxres = (maxresidaa[i] > maxres) ? maxresidaa[i] : maxres;
-				if (maxres < options - > term_precision) {
+				if (maxres < options -> term_precision) {
 					for (i = 1; i < numThreads; i++)
 						MPI_Isend( & maxres, 1, MPI_DOUBLE, i, 1, MPI_COMM_WORLD, & reqRes);
 				}
 			} else
 				MPI_Isend( & maxresiduum, 1, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD, & reqRes);
-			if (maxres < options - > term_precision)
+			if (maxres < options -> term_precision)
 				term_iteration = 0;
-		} else if (options - > termination == TERM_ITER) {
+		} else if (options -> termination == TERM_ITER) {
 			term_iteration--;
 		}
-		results - > stat_iteration++;
-		results - > stat_precision = maxresiduum;
+		results -> stat_iteration++;
+		results -> stat_precision = maxresiduum;
 	}
 
-	results - > m = m2;
+	results -> m = m2;
 }
 
 /* ************************************************************************ */
@@ -352,43 +332,43 @@ displayStatistics(struct calculation_arguments
 	const * arguments, struct calculation_results
 	const * results, struct options
 	const * options) {
-	int N = arguments - > N;
+	int N = arguments -> N;
 	double time = (comp_time.tv_sec - start_time.tv_sec) + (comp_time.tv_usec - start_time.tv_usec) * 1e-6;
 
 	printf("Berechnungszeit:	%f s \n", time);
-	printf("Speicherbedarf:	 %f MiB\n", (N + 1) * (N + 1) * sizeof(double) * arguments - > num_matrices / 1024.0 / 1024.0);
+	printf("Speicherbedarf:	 %f MiB\n", (N + 1) * (N + 1) * sizeof(double) * arguments -> num_matrices / 1024.0 / 1024.0);
 	printf("Berechnungsmethode: ");
 
-	if (options - > method == METH_GAUSS_SEIDEL) {
+	if (options -> method == METH_GAUSS_SEIDEL) {
 		printf("Gauß-Seidel");
-	} else if (options - > method == METH_JACOBI) {
+	} else if (options -> method == METH_JACOBI) {
 		printf("Jacobi");
 	}
 
 	printf("\n");
 	printf("Interlines:		 %"
-		PRIu64 "\n", options - > interlines);
+		PRIu64 "\n", options -> interlines);
 	printf("Stoerfunktion:	  ");
 
-	if (options - > inf_func == FUNC_F0) {
+	if (options -> inf_func == FUNC_F0) {
 		printf("f(x,y) = 0");
-	} else if (options - > inf_func == FUNC_FPISIN) {
+	} else if (options -> inf_func == FUNC_FPISIN) {
 		printf("f(x,y) = 2pi^2*sin(pi*x)sin(pi*y)");
 	}
 
 	printf("\n");
 	printf("Terminierung:	   ");
 
-	if (options - > termination == TERM_PREC) {
+	if (options -> termination == TERM_PREC) {
 		printf("Hinreichende Genaugkeit");
-	} else if (options - > termination == TERM_ITER) {
+	} else if (options -> termination == TERM_ITER) {
 		printf("Anzahl der Iterationen");
 	}
 
 	printf("\n");
 	printf("Anzahl Iterationen: %"
-		PRIu64 "\n", results - > stat_iteration);
-	printf("Norm des Fehlers:   %e\n", results - > stat_precision);
+		PRIu64 "\n", results -> stat_iteration);
+	printf("Norm des Fehlers:   %e\n", results -> stat_precision);
 	printf("\n");
 }
 
@@ -406,10 +386,10 @@ static
 void
 DisplayMatrix(struct calculation_arguments * arguments, struct calculation_results * results, struct options * options, int rank, int size, int from, int to) {
 	int
-	const elements = 8 * options - > interlines + 9;
+	const elements = 8 * options -> interlines + 9;
 
 	int x, y;
-	double ** Matrix = arguments - > Matrix[results - > m];
+	double ** Matrix = arguments -> Matrix[results -> m];
 	MPI_Status status;
 
 	/* first line belongs to rank 0 */
@@ -424,7 +404,7 @@ DisplayMatrix(struct calculation_arguments * arguments, struct calculation_resul
 		printf("Matrix:\n");
 
 	for (y = 0; y < 9; y++) {
-		int line = y * (options - > interlines + 1);
+		int line = y * (options -> interlines + 1);
 		if (rank == 0) {
 			if (line < from || line > to)
 				MPI_Recv(Matrix[0], elements, MPI_DOUBLE, MPI_ANY_SOURCE, 42 + y, MPI_COMM_WORLD, & status);
@@ -435,7 +415,7 @@ DisplayMatrix(struct calculation_arguments * arguments, struct calculation_resul
 
 		if (rank == 0) {
 			for (x = 0; x < 9; x++) {
-				int col = x * (options - > interlines + 1);
+				int col = x * (options -> interlines + 1);
 				if (line >= from && line <= to)
 					printf("%7.4f", Matrix[line][col]);
 				else
